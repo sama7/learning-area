816bf681b88cb944923c474fe5f78b20b7bdf356
Samah Saeed <samah.saeed@uconn.edu>
2022-09-27 22:56:04 -0400

    asynch animation assessment

--- javascript/asynchronous/sequencing-animations/start/main.js
+++ javascript/asynchronous/sequencing-animations/start/main.js
@@ -12,3 +12,30 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+// implementation 1: promise version of "callback hell"
+// alice1.animate(aliceTumbling, aliceTiming).finished
+//   .then(() => { alice2.animate(aliceTumbling, aliceTiming).finished
+//     .then(() => { alice3.animate(aliceTumbling, aliceTiming);
+//   })
+// });
+
+// implementation 2: promise chain
+alice1.animate(aliceTumbling, aliceTiming).finished
+  .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+  .then(() => alice3.animate(aliceTumbling, aliceTiming).finished)
+  .catch(error => console.error(`Error animating Alices: ${error}`));
+
+// implementation 3: async and await
+// async function animateAlices() {
+//   try {
+//     await alice1.animate(aliceTumbling, aliceTiming).finished;
+//     await alice2.animate(aliceTumbling, aliceTiming).finished;
+//     alice3.animate(aliceTumbling, aliceTiming);
+//   }
+//   catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// animateAlices();
